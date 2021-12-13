4b595afc5da06801125e22054b52f9030e2b99ff
Brain <zkzkzhzj>
2021-12-13 18:53:40 +0900

    strategy

---
+++ lib/src/main/java/com/zkzkzhzj/lib/strategy/Character.kt
@@ -0,0 +1,6 @@
+package com.zkzkzhzj.lib.strategy
+
+// Character 추상 클래스
+//https://velog.io/@y_dragonrise/%EB%94%94%EC%9E%90%EC%9D%B8-%ED%8C%A8%ED%84%B4-%EC%A0%84%EB%9E%B5-%ED%8C%A8%ED%84%B4Strategy-Pattern
+abstract class Character {
+}
